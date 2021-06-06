d066add4ea5f2ce0fa444fdc93fcfcf078b596b9
Rafael Langthaler <langthaler.r@gmx.at>
2021-06-06 10:36:39 +0200

    basemap und map config added

---
+++ main.js
@@ -0,0 +1,24 @@
+//basemap Hintergruende definieren
+
+let baselayers = {
+    standard: L.tileLayer.provider("BasemapAT.basemap"),
+    grau: L.tileLayer.provider("BasemapAT.grau"),
+    terrain: L.tileLayer.provider("BasemapAT.terrain"),
+    surface: L.tileLayer.provider("BasemapAT.surface"),
+    highdpi: L.tileLayer.provider("BasemapAT.highdpi"),
+    ortho_overlay: L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ]),
+};
+
+// Karte initialisieren und auf Wiens Wikipedia Koordinate blicken
+let map = L.map("map", {
+    fullscreenControl: true,
+    center: [47.59397, 14.12456],
+    zoom: 10,
+    layers: [
+        baselayers.grau
+    ]
+});
+
