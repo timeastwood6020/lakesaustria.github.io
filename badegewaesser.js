8be4e6c7018622ad3e5055b4c8bcf9f579eaec2e
Rafael Langthaler <langthaler.r@gmx.at>
2021-06-06 10:13:13 +0200

    badegewaesser array created

--- badegewaesser.js
+++ badegewaesser.js
@@ -1,4 +1,8 @@
 // https://www.ages.at/typo3temp/badegewaesser_db.json
 // https://www.data.gv.at/katalog/dataset/badegewaesser/resource/7a90ef57-8cb2-4d62-a474-aa2085733de9
 
-
+const BADEGEWAESSER = [{
+    title: "",
+    source: "",
+    data: ""
+}]
