1907a6168d21949bfc7babaec5fd6ec64122006c
csnwolf <christianwolf021@gmail.com>
2021-06-08 23:15:49 +0200

    Pin und Popup für Badestellen erstellt

--- wien.js
+++ wien.js
@@ -13,3 +13,13 @@ let map = L.map("map", {
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray
 }).addTo(map);
+
+fetch("data/BADESTELLENOGD.json")
+.then(response => response.json())
+.then(stations => {
+    L.geoJson(stations, {
+        onEachFeature: (feature, layer) => {
+            layer.bindPopup(feature.properties.BEZEICHNUNG)
+        }
+    }).addTo(map);
+})
