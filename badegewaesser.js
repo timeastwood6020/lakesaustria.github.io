74289a6c13cf7d9918a979911d8da56d9315e791
Rafael Langthaler <langthaler.r@gmx.at>
2021-06-08 10:30:28 +0200

    semicolon added

--- badegewaesser.js
+++ badegewaesser.js
@@ -5,4 +5,5 @@ const BADEGEWAESSER = [{
     title: "Badegewaesser Oesterreich",
     source: "https://www.data.gv.at/katalog/dataset/badegewaesser/resource/7a90ef57-8cb2-4d62-a474-aa2085733de9",
     data: "data/badegewaesser_db.json"
-}]
+}];
+
