b7310d4d32b39958303aaac68ca8518ef25ceb84
csnwolf <christianwolf021@gmail.com>
2021-06-08 23:01:55 +0200

    Daten für Badestellen und Grillplätze eingefügt

---
+++ ogdwien.js
@@ -0,0 +1,9 @@
+const OGDWIEN = [{
+    title: "Badestellen Standorte Wien",
+    source: "https://www.data.gv.at/katalog/dataset/7c6e9dbf-487b-4146-a379-19e46642c392",
+    data: "data/BADESTELLENOGD.json"
+}, {
+    title: "Grillplätze Standorte Wien",
+    source: "https://www.data.gv.at/katalog/dataset/c6fc4688-1aca-4363-bd1a-8d14ea08d2f8",
+    data: "data/GRILLPLATZOGD.json"
+}];
